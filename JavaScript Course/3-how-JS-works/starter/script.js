e332f5bd3e00898046503b5a6b5d8d497c2eb464
lukasmvv <lukasmvv@gmail.com>
2020-01-17 09:40:15 +0000

    section3 done

--- JavaScript Course/3-how-JS-works/starter/script.js
+++ JavaScript Course/3-how-JS-works/starter/script.js
@@ -2,10 +2,33 @@
 // Lecture: Hoisting
 
 
+// Hoisting with function declarations
+// Using a function before it is declared - this is possible thanks to hoisting
+calcAge(1965);
+
+function calcAge(year) {
+    console.log(2020-year);
+}
+
+// Using function after declaring it
+calcAge(1990);
 
 
+// Hoisting with funtcion expressions
+
+//retirement(1965);
+
+var retirement = function(year) {
+    console.log(65 - (2020 - year));
+}
+
+retirement(1990);
 
 
+// variables
+console.log(age);
+var age = 23;
+console.log(age);
 
 
 
