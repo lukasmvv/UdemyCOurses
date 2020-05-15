b42c6b51885515bed85cf3e2d9a2344d278f14a9
lukasmvv@gmail.com <lukasmvv@gmail.com>
2020-05-15 16:29:40 +0100

    section 11 done

--- React - The Complete Guide/http-01-starting-setup/http--01-starting-setup/src/containers/Blog/Blog.js
+++ React - The Complete Guide/http-01-starting-setup/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,61 +1,50 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
+//import NewPost from '../NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import FullPost from '../Blog/FullPost/FullPost';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import './Blog.css';
 
+// this is for so called Lazy Loading
+// the import() is imported only when it is called
+// idea is that a user might not need/use a large part of app that is imported - this helps to save space and increase performance
+// React 16.6 and later has lazy loading built in which can be used
+// const Posts = React.lazy(() => import('./containers/Posts'));
+const AsyncNewPost = asyncComponent(() => {
+    return import('../NewPost/NewPost');
+});
+
 class Blog extends Component {
-
-    state= {
-        posts: [],
-        selectedPostId: null,
-        error: false
+    state = {
+       auth: true 
     }
-
-    componentDidMount() {
-        axios.get('/posts').then(response => {
-            //console.log(response);
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post, 
-                    author: 'Max'
-                }
-            });
-            this.setState({posts: updatedPosts});
-        }).catch(error => {
-            console.log(error);
-            this.setState({error: true})
-        });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render () {
 
-        let posts = <p style={{textAlign: "center"}}>Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>
-            });
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/posts/" exact={true} activeClassName="active">Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post', // this is always an aboslute path - gets appended to root domain
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact={true} render={()=><Posts/>}/> */}
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}  
+                    <Route path="/posts" component={Posts}/>   
+                    <Route render={() => <h1>Not found</h1>}></Route>
+                    {/* <Redirect from="/" to="/posts"></Redirect>                                                 */}
+                    {/* <Route path="/" component={Posts}/>  */}
+                </Switch>
             </div>
         );
     }
---
+++ React - The Complete Guide/http-01-starting-setup/http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+import {Route, Link} from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
+
+class Posts extends Component {
+    state= {
+        posts: []
+    }
+    
+    postSelectedHandler = (id) => {
+        //this.setState({selectedPostId: id});
+        this.props.history.push({pathname: '/posts/'+id});
+        //this.props.history.push('/posts'+id);  navigating programmatically
+    }
+
+    componentDidMount() {
+        //console.log(this.props);
+        axios.get('/posts').then(response => {
+            //console.log(response);
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post, 
+                    author: 'Max'
+                }
+            });
+            this.setState({posts: updatedPosts});
+        }).catch(error => {
+            console.log(error);
+            this.setState({error: true})
+        });
+    }
+
+    render() {
+
+        let posts = <p style={{textAlign: "center"}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return ( 
+                    //<Link  to={'/posts/'+post.id}>
+                        <Post       
+                            key={post.id}                       
+                            title={post.title} 
+                            author={post.author} 
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+                    //</Link>
+                )
+            });
+        }
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url+"/:id"} exact={true} component={FullPost}/>
+            </div>            
+        );
+    }
+}
+
+export default Posts;
